39de4a76e33c19ae88e9ef63f6ae63195f9f5709
david p <david@foo.bar>
2017-11-07 14:07:49 +0100

    added firebase config files

---
+++ template/src/config/test.firebase.config.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * example for your firebase configuration
+ * replace fields as needed
+ *
+ * @type {Object}
+ */
+module.exports = {
+	projectId: '<project-id>',
+	appName: '<app-name>',
+	apiKey: '<api-key>',
+	authDomain: '<project-id>.firebaseapp.com',
+	databaseURL: 'https://<project-id>.firebaseio.com',
+	storageBucket: '<project-id>.appspot.com',
+}
